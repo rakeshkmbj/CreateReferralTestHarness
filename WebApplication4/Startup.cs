0dd544985e4cbffd3819597bb655d9d073f50a17
vlubarda <vlubarda@nbty.com>
2018-06-13 09:59:18 -0400

    add all

---
+++ WebApplication4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CreateReferralTestHarness.Startup))]
+namespace CreateReferralTestHarness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
